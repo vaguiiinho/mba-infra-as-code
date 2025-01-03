463ed184a1cd1513fd8bd2f404126bb7d6f62a43
vaguiiinho <avanlendario@gmail.com>
2025-01-03 16:32:49 -0300

    Habilitando acesso a VM

--- aws/main.tf
+++ aws/main.tf
@@ -31,13 +31,14 @@ resource "aws_subnet" "example_subnet" {
 }
 
 resource "aws_instance" "example_instance" {
-  ami           = "ami-07d9cf938edb0739b"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.example_subnet.id
+  ami                    = "ami-07d9cf938edb0739b"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.example_subnet.id
+  vpc_security_group_ids = [aws_security_group.example_sg.id]
 
   user_data = <<EOF
   #!/bin/bash
-  DB_STRING="Serve=${jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["Host"]}; DB=${jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["Db"]}"
+  DB_STRING="Server=${jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["Host"]};DB=${jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["Db"]}"
   echo $DB_STRING > test.txt
   EOF
 }
@@ -55,6 +56,43 @@ resource "aws_ssm_parameter" "parameter" {
   name  = "mv_ip"
   type  = "String"
   value = aws_eip.example_ip.public_ip
+}
+
+resource "aws_route_table" "example_route_table" {
+  vpc_id = aws_vpc.example_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.example_igw.id
+  }
+}
+
+resource "aws_route_table_association" "example_association" {
+  subnet_id      = aws_subnet.example_subnet.id
+  route_table_id = aws_route_table.example_route_table.id
+}
+
+resource "aws_security_group" "example_sg" {
+  vpc_id = aws_vpc.example_vpc.id
+  name   = "Allow SSH"
+
+  tags = {
+    name = "Allow SSH"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "example_sg_ingress_rule" {
+  security_group_id = aws_security_group.example_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "example_sg_egress_rule" {
+  security_group_id = aws_security_group.example_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
 }
 
 output "private_dns" {
