b4d1e0af902944fa017b68cbecd4ab0eeeb8977f
vaguiiinho <avanlendario@gmail.com>
2025-01-03 15:03:15 -0300

    Declarando os recursos - Parte 2

--- aws/main.tf
+++ aws/main.tf
@@ -1,14 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.82.2"
     }
   }
 }
 
 provider "aws" {
-  region     = "us-west-2"
+  region  = "us-west-2"
   profile = "default"
 }
 
@@ -17,13 +17,36 @@ resource "aws_vpc" "example_vpc" {
 }
 
 resource "aws_subnet" "example_subnet" {
-  vpc_id = aws_vpc.example_vpc.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id            = aws_vpc.example_vpc.id
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "us-west-2a"
 }
 
 resource "aws_instance" "example_instance" {
-  ami = "ami-07d9cf938edb0739b"
+  ami           = "ami-07d9cf938edb0739b"
   instance_type = "t2.micro"
-  subnet_id = aws_subnet.example_subnet.id
+  subnet_id     = aws_subnet.example_subnet.id
+}
+
+resource "aws_internet_gateway" "example_igw" {
+  vpc_id = aws_vpc.example_vpc.id
+}
+
+resource "aws_eip" "example_ip" {
+  instance   = aws_instance.example_instance.id
+  depends_on = [aws_internet_gateway.example_igw]
+}
+
+resource "aws_ssm_parameter" "parameter" {
+  name = "mv_ip"
+  type = "String"
+  value = aws_eip.example_ip.public_ip
+}
+
+output "private_dns" {
+  value = aws_instance.example_instance.private_dns
+}
+
+output "eip" {
+  value = aws_eip.example_ip.public_ip
 }
