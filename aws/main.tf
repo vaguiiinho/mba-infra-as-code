d2e0b34f323e70d3df613eafdd69041e487f2d71
vaguiiinho <avanlendario@gmail.com>
2025-01-03 14:49:08 -0300

    Declarando os recursos -parte 1

--- aws/main.tf
+++ aws/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.49.2"
+      version = "5.82.2"
     }
   }
 }
@@ -10,4 +10,20 @@ terraform {
 provider "aws" {
   region     = "us-west-2"
   profile = "default"
+}
+
+resource "aws_vpc" "example_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "example_subnet" {
+  vpc_id = aws_vpc.example_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+}
+
+resource "aws_instance" "example_instance" {
+  ami = "ami-07d9cf938edb0739b"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.example_subnet.id
 }
